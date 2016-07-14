949409a6babc0f3a25f1b6e0776d226f4f185abe
Aist Zulix <spamermail@rambler.ru>
2016-07-14 12:27:50 +0300

    part of Array statements

--- src/first/Task2021.java
+++ src/first/Task2021.java
@@ -3,25 +3,21 @@ package first;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-import java.util.Arrays;
 import java.util.Scanner;
 
 /**
  * Created by user on 13.07.16.
  */
-public class Task2020 {
+public class Task2021 {
     public static void main(String[] args) {
-        File file = new File("texts/input_2020.txt");
+        File file = new File("texts/input_2021.txt");
         if(!file.exists()){
             System.out.println("Cannot open file");
             System.exit(1);
         }
 
         try(Scanner input = new Scanner(file)){
-            int sum = 0;
             int count = 0;
-            int num = 0;
-            int amount = 0;
             while(input.hasNext()){
                 String line = input.nextLine();
                 try {
@@ -30,22 +26,29 @@ public class Task2020 {
                     else {
                         int []numbers = getInteger(line.split(" "));
                         if(numbers!=null && numbers.length>0) {
-                            num = numbers[0];
-                            amount = 1;
-                            int tempNum = num;
-                            int tempAmount = amount;
-                            for (int i = 1; i < numbers.length; i++) {
-                                if(tempNum == numbers[i]){
-                                    ++tempAmount;
-                                }else{
-                                    if(tempAmount>amount){
-                                        amount = tempAmount;
-                                        num = tempNum;
-                                    }
-                                    tempAmount = 1;
-                                    tempNum = numbers[i];
+                            int maxIndex = 0;
+
+                            if(count>numbers.length)
+                                maxIndex = numbers.length;
+                            else
+                                maxIndex = count;
+
+
+
+                            PrintWriter output = new java.io.PrintWriter("texts/output_2021.txt");
+                            for (int j = 0; j <2 ; j++) {
+                                for (int i = 0; i <maxIndex ; i++) {
+                                    if(numbers[i]>2)
+                                        numbers[i]=numbers[i]/2;
+
                                 }
                             }
+                            for (int i = 0; i <maxIndex ; i++) {
+                                output.print(numbers[i]+" ");
+
+                            }
+
+                            output.close();
                         }
                         break;
                     }
@@ -54,20 +57,16 @@ public class Task2020 {
                 }
             }
 
-            try (PrintWriter output = new java.io.PrintWriter("texts/output_2020.txt")) {
-                output.println(num +"  "+amount);
-            }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             System.out.println("Exception: Cannot open file");
         }
     }
-
     public static int[] getInteger(String [] arr){
         int [] numbers = new int[arr.length];
         for (int i = 0; i < arr.length; i++) {
             numbers[i] = Integer.parseInt(arr[i]);
-            if(numbers[i]<1 || numbers[i]>10000) {
+            if(numbers[i]<1 || numbers[i]>100) {
                 numbers = null;
                 break;
             }
---
+++ src/first/Task2026.java
@@ -0,0 +1,68 @@
+package first;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * Created by user on 14.07.16.
+ */
+public class Task2026 {
+    public static void main(String[] args) {
+        File file = new File("texts/input_2026.txt");
+        if(!file.exists()){
+            System.out.println("Cannot open file");
+            System.exit(1);
+        }
+
+        try(Scanner input = new Scanner(file)){
+            int count = 0;
+            while(input.hasNext()){
+                String line = input.nextLine();
+                try {
+                    if(count == 0)
+                        count = Integer.parseInt(line);
+                    else {
+                        int []numbers = Task2020.getInteger(line.split(" "));
+                        if(numbers!=null && numbers.length>0) {
+                            int maxIndex = 0;
+
+                            if(count>numbers.length)
+                                maxIndex = numbers.length;
+                            else
+                                maxIndex = count;
+
+
+
+                            PrintWriter output = new java.io.PrintWriter("texts/output_2026.txt");
+                            for (int j = 0; j < numbers.length; j++) {
+                                boolean flag = true;
+                                for (int i = j; i < numbers.length; i++) {
+                                    if (numbers[j] < numbers[i]) {
+                                        numbers[j] = numbers[i];
+                                        flag = false;
+                                        break;
+                                    }
+                                }
+                                if(flag){
+                                    numbers[j] = 0;
+                                }
+                                output.print(numbers[j]+" ");
+                            }
+
+                            output.close();
+                        }
+                        break;
+                    }
+                }catch (NumberFormatException ne){
+
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("Exception: Cannot open file");
+        }
+    }
+}
